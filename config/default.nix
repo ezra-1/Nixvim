d9999cc3764ab6932bcaa5257497cccc0184e798
Ezra Lawrence <ezralawrence03@gmail.com>
2026-05-12 12:29:33 -0500

    chore(nixvim): enable navigation and editing enhancements

---
+++ config/default.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+
+{
+  # Core modules only
+  imports = [
+    ./core
+  ];
+
+  # Aliases
+  viAlias = true;
+  vimAlias = true;
+
+  # Leader key
+  globals.mapleader = " ";
+
+  # Clipboard (Wayland only)
+  clipboard = {
+    register = "unnamedplus";
+    providers.wl-copy.enable = true;
+  };
+
+  # Plugins
+  plugins = {
+    persistence.enable = true;
+    direnv.enable = true;
+    web-devicons.enable = true;
+    lastplace.enable = true;
+    nvim-autopairs.enable = true;
+
+    tmux-navigator.enable = true;
+
+    better-escape = {
+      enable = true;
+      settings.timeout = 200;
+    };
+
+    zellij = {
+      enable = true;
+      settings.vimTmuxNavigatorKeybinds = true;
+    };
+  };
+}
